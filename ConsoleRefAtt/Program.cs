76c8220370ee71f73a8fb36cfd15d2d66584995a
Mertcan Dursun <221903570@ogr.uludag.edu.tr>
2022-04-19 14:15:46 +0300

    Last Commit

---
+++ ConsoleRefAtt/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRefAtt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per1 = new Person()
+            {
+                Name = "Mert",
+                Age = 22,
+            };
+
+            Console.WriteLine(Control.check(per1));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        [Required]
+        public string Id;
+        [Required]
+        public string Name;
+        [Required]
+        public int Age;
+    }
+
+    public class RequiredAttribute : Attribute
+    {
+
+    }
+
+    public static class Control
+    {
+        public static bool check(Person per)
+        {
+            Type type = per.GetType();
+            foreach (var items in type.GetFields())
+            {
+                object[] attribute = items.GetCustomAttributes(typeof(RequiredAttribute), true);
+                if (attribute.Length != 0)
+                {
+                    object value = items.GetValue(per);
+
+                    if (value == null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
